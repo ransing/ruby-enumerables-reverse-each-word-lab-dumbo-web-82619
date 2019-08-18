799ce0088c2ef755ed42f50fa1b96af9408d0b9e
virajransing <virajransing@gmail.com>
2019-08-18 15:05:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ")
   reversed_words = array.collect do |word|
-    word.reversed_words
+    word.reverse
   end
   reversed_words.join("")
 end 
